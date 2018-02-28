1e92509c2c3677f153c272268a66ea655a30c00e
pauloyasu <paulohyy@gmail.com>
2018-02-27 23:11:59 -0300

    Create Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,6 @@
+public static class Constants
+{
+    public static Vector3 Zero = new Vector3(0, 0, 0);
+    public static Vector3 Unit = new Vector3(1, 1, 1);
+    public static Vector3 Bottom = new Vector3(0, -1000, 0);
+}
