5212fbe682745e20ff983d122eb5f859c75b4db2
pauloyasu <paulohyy@gmail.com>
2018-02-27 23:26:29 -0300

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -1,6 +1,11 @@
-public static class Constants
+using Godot;
+
+namespace SkipTheBadEngine
 {
-    public static Vector3 Zero = new Vector3(0, 0, 0);
-    public static Vector3 Unit = new Vector3(1, 1, 1);
-    public static Vector3 Bottom = new Vector3(0, -1000, 0);
+    public static class Constants
+    {
+        public static Vector3 Zero = new Vector3(0, 0, 0);
+        public static Vector3 Unit = new Vector3(1, 1, 1);
+        public static Vector3 Bottom = new Vector3(0, -1000, 0);
+    }
 }
